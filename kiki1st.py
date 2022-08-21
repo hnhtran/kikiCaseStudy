ab6fb2318e3a8825c88524518522a07de4702940
hnhtran <tran_eetas@yahoo.com>
2022-08-22 01:28:50 +1000

    first rough solution

--- kiki1st.py
+++ kiki1st.py
@@ -1,6 +1,3 @@
-from struct import pack
-
-
 code10 = 'OFR001'
 code7 = 'OFR002'
 code5 = 'OFR003'
@@ -67,7 +64,7 @@ class KikiShop:
         Finish delivery comeback, deliver again (2*time)
         """
         # pkgs = self.pkgs
-        pkgs = [{'pkg_id1': 'PKG1', 'pkg_weight1': 50, 'pkg_distance1': 30, 'offer_code1': 'OFR001', 'discount1': 0, 'total_cost1': 700}, {'pkg_id2': 'PKG2', 'pkg_weight2': 75, 'pkg_distance2': 125, 'offer_code2': 'OFR008', 'discount2': 0, 'total_cost2': 1475}, {'pkg_id3': 'PKG3', 'pkg_weight3': 175, 'pkg_distance3': 100, 'offer_code3': 'OFR003', 'discount3': 0, 'total_cost3': 2350}, {'pkg_id4': 'PKG4', 'pkg_weight4': 110, 'pkg_distance4': 60, 'offer_code4': 'OFR002', 'discount4': 105, 'total_cost4': 1395}, {'pkg_id5': 'PKG5', 'pkg_weight5': 155, 'pkg_distance5': 95, 'offer_code5': 'NA', 'discount5': 0, 'total_cost5': 2125}]
+        pkgs = [{'pkg_id1': 'PKG1', 'pkg_weight1': 50, 'pkg_distance1': 20, 'offer_code1': 'OFR001', 'discount1': 0, 'total_cost1': 700}, {'pkg_id2': 'PKG2', 'pkg_weight2': 75, 'pkg_distance2': 125, 'offer_code2': 'OFR008', 'discount2': 0, 'total_cost2': 1475}, {'pkg_id3': 'PKG3', 'pkg_weight3': 175, 'pkg_distance3': 100, 'offer_code3': 'OFR003', 'discount3': 0, 'total_cost3': 2350}, {'pkg_id4': 'PKG4', 'pkg_weight4': 110, 'pkg_distance4': 60, 'offer_code4': 'OFR002', 'discount4': 105, 'total_cost4': 1395}, {'pkg_id5': 'PKG5', 'pkg_weight5': 155, 'pkg_distance5': 95, 'offer_code5': 'NA', 'discount5': 0, 'total_cost5': 2125}]
         max_weight = self.max_carriage_weight
         speed = self.max_speed
         vehicles = self.no_of_vehicles
@@ -92,73 +89,76 @@ class KikiShop:
             weight = pkg['pkg_weight{}'.format(i+1)]
             time = distance / speed
             weight_objs[weight] = {'index': i, 'pkg_id': pkg_id, 'time': time, 'weight': weight, 'distance': distance}
+            # print(f'type of weight_ob[weight] is {type(int(weight_obj[weight]))}')
         
         to_deliver_pkg = {}
-        total_distance = 0
-        def package_to_deliver(self):
+        print(weight_objs)
+        def assign_driver(self, to_deliver_pkg, total_weight):
+                min_avail = max_weight - total_weight
+                weight_obj = {}
+                max_obj = {}
+                print(min_avail)
+                for key in weight_objs.keys():
+                    if key <= min_avail:
+                        weight_obj[key] = weight_objs[key]
+                print(max_obj)
+                    # min_avail = max_weight - max(weight_objs.keys())
+                print(min_avail)
+                print(weight_obj)
+                if len(weight_obj) == 0 and len(to_deliver_pkg) == 0:
+                    key = max(weight_objs.keys())
+                    # print(key)
+                    to_deliver_pkg[key] = weight_objs.pop(key)
+                elif len(weight_obj) > 0:
+                    temp1 = max(weight_objs.keys())
+                    temp2 = max(weight_obj.keys())
+                    distance1 = weight_objs[temp1]['distance']
+                    distance2 = weight_objs[temp2]['distance']
+                    to_deliver_pkg[distance1] = weight_objs.pop(temp1)
+                    to_deliver_pkg[distance2] = weight_objs.pop(temp2)
+                    total_weight = temp1 + temp2
+                    weight_obj.clear()
+                    assign_driver(self, to_deliver_pkg, total_weight)
+                if len(to_deliver_pkg) == 0:
+                    return
+                driver = {}
+                for i in range(len(drivers)):
+                    if drivers[i]['available'] == True:
+                        driver = drivers[i]
+                        break
+                    mintime = min(drivers, key=lambda x: x['available_time'])
+                    if drivers[i]['available_time'] == mintime['available_time']:
+                        # print(len(to_deliver_pkg))
+                        drivers[i]['available'] = True
+                        driver = drivers[i]
+                        break
+                    
+                print(driver)
+                time = 0
+                time_total = 0
+                for i in range(len(to_deliver_pkg)):
+                    driver['pkgs'].append(to_deliver_pkg[min(to_deliver_pkg.keys())]['pkg_id'])
+                    pkg = to_deliver_pkg[min(to_deliver_pkg.keys())]
+                    time = pkg['time']
+                    driver['available'] = False
+                    index = pkg['index']
+                    pkgs[index]['delivered'] = True 
+                    pkgs[index]['driver'] = driver['driver']
+                    pkgs[index][f'estimated_delivery_time{index+1}_in_hours'] = time + driver['available_time']
+                    to_deliver_pkg.pop(min(to_deliver_pkg.keys()))
+                driver['available_time'] += time * 2
+                # print(weight_objs)
+        max_of_weight = 0
+        for i in range(len(weight_objs)):
+            # print(len(weight_objs))
+            max_of_weight = max(weight_objs.keys())
+            assign_driver(self, to_deliver_pkg, max_of_weight)
+            print(len(weight_objs))
             if len(weight_objs) == 1:
-                return
-            max_weight_obj = max(weight_objs)
-            temp_weight_obj = 0
-            min_avail = max_weight - max_weight_obj
-            for weight in weight_objs:
-                if weight <= min_avail:
-                    temp_weight_obj = weight
-                    if weight > temp_weight_obj:
-                        temp_weight_obj = weight
-            if temp_weight_obj != 0:
-                distance1 = weight_objs[max_weight_obj]['distance']
-                to_deliver_pkg[distance1] = weight_objs.pop(max_weight_obj)
-                distance2 = weight_objs[temp_weight_obj]['distance']
-                to_deliver_pkg[distance2] = weight_objs.pop(temp_weight_obj)
-                total_weight = max_weight_obj + temp_weight_obj
-                print(total_weight)
-                # print(len(weight_obj))
-                # print(max(weight_obj.keys()))
-                if len(weight_obj) > 0 and total_weight < max(weight_obj.keys()):
-                    to_deliver_pkg.clear()
-                    temp = max(weight_obj.keys())
-                    to_deliver_pkg[temp] = weight_obj.pop(max(weight_obj.keys()))
-            if len(to_deliver_pkg) == 0:
-                weight_obj[max_weight_obj] = weight_objs.pop(max(weight_objs))
-            package_to_deliver(self)
-            
-        def asign_driver(self, package_to_deliver):
-            print(package_to_deliver)
-            for i in range(len(drivers)):
-                if drivers[i]['available'] == True:
-                    driver = drivers[i]
-                    break
-                mintime = min(drivers, key=lambda x: x['available_time'])
-                if drivers[i]['available_time'] == mintime['available_time']:
-                    # print(len(to_deliver_pkg))
-                    drivers[i]['available'] = True
-                    driver = drivers[i]
-                    break
-            # print(driver)
-            time = 0
-            for i in range(len(package_to_deliver)):
-                driver['pkgs'].append(package_to_deliver[min(package_to_deliver.keys())]['pkg_id'])
-                pkg = package_to_deliver[min(package_to_deliver.keys())]
-                time = pkg['time']
-                driver['available'] = False
-                index = pkg['index']
-                pkgs[index]['delivered'] = True 
-                pkgs[index]['driver'] = driver['driver']
-                pkgs[index][f'estimated_delivery_time{index+1}_in_hours'] = time + driver['available_time']
-                package_to_deliver.pop(min(package_to_deliver.keys()))
-            driver['available_time'] += time * 2
-        package_to_deliver(self)
-        print(to_deliver_pkg)
-        print(len(weight_objs))
-        asign_driver(self, to_deliver_pkg)
-        print(to_deliver_pkg)
-        tempobj = {}
-        for key in weight_obj.keys():
-            print(weight_obj[key]['distance'])
-            tempobj[weight_obj[key]['distance']] = weight_obj[key]
-            # print(tempobj)
-            asign_driver(self, tempobj)
+                break
+        print(drivers)
+        print(max(weight_objs.keys()))
+        print(len(to_deliver_pkg))
         to_deliver_pkg = weight_objs[max(weight_objs.keys())]
         # print(to_deliver_pkg)
         driver = {}
@@ -172,11 +172,8 @@ class KikiShop:
                 driver['available_time'] += to_deliver_pkg['time'] * 2
         weight_objs.clear()
         to_deliver_pkg.clear()
-        # package_to_deliver(self)
-        print(weight_objs)
         print(drivers)
-
-            
+        print(pkgs)
 
 deliveryCost = KikiShop(100, 5, 2, 70, 200)
 # print(deliveryCost)
